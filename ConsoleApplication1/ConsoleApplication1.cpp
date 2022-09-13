86b4e7b1c42d1730866b97e54b6fcc50902f5dc5
Halv <Halv@DESKTOP-47UDAT9>
2022-09-12 19:05:06 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "sodium.h"
+
+/*#define MESSAGE ((const unsigned char *) "Arbitrary data to hash")
+#define MESSAGE_LEN 22*/
+
+int main(void)
+{
+    unsigned char buf[128];
+    if (sodium_init() == -1) {
+        return 1;
+    }
+    //printf("Hello World\n");
+    randombytes_buf(buf, sizeof((buf)));
+    printf("%lu\n", sizeof(buf));
+    for (int i = 0; i < sizeof(buf); i++)
+        printf("%02x ", (unsigned int)buf[i]);
+    printf("\n");
+    /*
+    unsigned char hash[crypto_generichash_BYTES];
+    crypto_generichash(hash, sizeof hash,*/
+}
